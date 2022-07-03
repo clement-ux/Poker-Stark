ec52882db389d2f2cc61fc37c7e213034ff943e7
LucasRamadour <72853747+LucasRamadour@users.noreply.github.com>
2022-07-03 19:23:37 +0200

    Amélioration du bouton sidebar

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -9,7 +9,7 @@ export const Layout = ({children} :any) => {
             <Header/>
             <div className="flex h-[95vh] w-full">
                 <div className={`flex ${showSide ? "w-64" : "w-14"} bg-gray-400 pl-2 ease-in duration-300`}>
-                    <div className="flex bg-gray-500 hover:bg-gray-600 rounded-[3px] h-10 w-10 items-center justify-center cursor-pointer" onClick={() => setShowSide(!showSide)}>
+                    <div className="flex bg-gray-400 hover:bg-gray-500 rounded-[3px] h-10 w-10 items-center justify-center cursor-pointer" onClick={() => setShowSide(!showSide)}>
                         <AiOutlineMenu size={20}/>
                     </div>
                 </div>
