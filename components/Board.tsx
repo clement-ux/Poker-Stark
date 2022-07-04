8f71c491cf700e8d4188660358c6f1522a7e9994
LucasRamadour <72853747+LucasRamadour@users.noreply.github.com>
2022-07-05 00:09:02 +0200

    river first steps

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
 import {Card} from './Card'
 
+const colors = ["H", "D", "C", "S"];
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
 export const Board = () => {
 
-    const [betValue, setBetValue] = useState('0')
+    const [step, setStep] = useState('pre-flop');
+
+    const [betValue, setBetValue] = useState('0');
+
+    const [riverCards, setRiverCards] = useState([<div/>]);
 
     const Bet = (value : string) => {
         console.log("bet " + value)
@@ -17,30 +24,78 @@ export const Board = () => {
         console.log("Fold")
     }
 
+    const nextStep = (step : string) =>{
+        switch(step){
+            case 'pre-flop':
+                const drawValues = [values[Math.floor(Math.random() * 13)], values[Math.floor(Math.random() * 13)], values[Math.floor(Math.random() * 13)]];
+                console.log(drawValues)
+                const drawColors = [Math.floor(Math.random() * 4), Math.floor(Math.random() * 4), Math.floor(Math.random() * 4)];
+                setRiverCards(drawValues.map((value, index)=><Card value={value} symbol={colors[drawColors[index]]} size={8}/>));
+                console.log(riverCards)
+                setStep('flop');
+                break;
+            case 'flop':
+                setRiverCards([...riverCards, <Card value={values[Math.floor(Math.random() * 13)]} symbol={colors[Math.floor(Math.random() * 4)]} size={8}/>]);
+                setStep('turn');
+                break;
+            case 'turn':
+                setRiverCards([...riverCards, <Card value={values[Math.floor(Math.random() * 13)]} symbol={colors[Math.floor(Math.random() * 4)]} size={8}/>]);
+                setStep('river');
+                break;
+            case 'river': 
+                setRiverCards([<div/>]);
+                setStep('pre-flop');
+                break;
+
+        }
+            
+    }
+
     return(
         <div className='h-full w-full'>
             <div className='h-3/4'>
+                <div className='h-1/3'>
 
+                </div>
+                <div className='flex flex-row h-2/3 w-full justify-between items-center '>
+                    <div className='flex w-1/4 justify-center items-center'>
+                        <Card flipped size={8}/>
+                    </div>
+                    <div className='flex w-2/4 justify-start items-center space-x-4'>
+                        {riverCards}
+                    </div>
+                    <div className='flex w-1/4 justify-center items-center'>
+                        <div className='flex h-12 w-12 rounded-[1000px] bg-green-400 justify-center items-center cursor-pointer' onClick={()=>nextStep(step)}>
+                            Next
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className='flex flex-row h-1/4'>
                 <div className='w-2/5'>
 
                 </div>
                 <div className='flex flex-row space-x-4 w-1/5'>
-                    <Card value='10' symbol='S'/>
-                    <Card value='K' symbol='D'/>
+                    <Card value='7' symbol='C' flippable/>
+                    <Card value='2' symbol='S' flippable/>
                 </div>
                 <div className='flex flex-row w-2/5 justify-evenly items-center'>
                     <input placeholder='0' className='border-2' onChange={(e)=>setBetValue(e.target.value)}/>
-                    <div className='flex h-32 w-32 rounded-[1000px] bg-sky-400 cursor-pointer justify-center items-center' onClick={()=>Bet(betValue)}>
-                        <p>Bet</p>
+                    <div className='grid grid-cols-2 gap-5'>
+                        <div className='flex row-span-2 justify-center items-center'>
+                            <div className='flex h-32 w-32 rounded-[1000px] bg-sky-400 cursor-pointer justify-center items-center' onClick={()=>Bet(betValue)}>
+                                <p>Bet</p>
+                            </div>
+                        </div>
+                        
+                        <div className='flex h-32 w-32 rounded-[1000px] bg-gray-400 cursor-pointer justify-center items-center' onClick={()=>Follow()}>
+                            <p>Check/Follow</p>
+                        </div>
+                        <div className='flex h-32 w-32 rounded-[1000px] bg-red-400 cursor-pointer justify-center items-center' onClick={()=>Fold()}>
+                            <p>Fold</p>
+                        </div>
                     </div>
-                    <div className='flex h-32 w-32 rounded-[1000px] bg-gray-400 cursor-pointer justify-center items-center' onClick={()=>Follow()}>
-                        <p>Check/Follow</p>
-                    </div>
-                    <div className='flex h-32 w-32 rounded-[1000px] bg-red-400 cursor-pointer justify-center items-center' onClick={()=>Fold()}>
-                        <p>Fold</p>
-                    </div>
+                    
                 </div>
                 
             </div>
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
 import { ImClubs, ImDiamonds, ImHeart, ImSpades } from 'react-icons/im'
 
-export const Card = ({value, symbol} : any) => {
+export const Card = ({value, symbol, size, flippable, flipped} : any) => {
 
-    const [isFlipped, setIsFlipped] = useState(false)
+    if(!size){
+        size = 10
+    }
+
+    const height = (2*size).toString() 
+    const width = (1.4*size).toString() 
+
+    const [isFlipped, setIsFlipped] = useState(flipped)
 
     const Flip = () => {
-        setIsFlipped(!isFlipped)
+        if(flippable){
+            setIsFlipped(!isFlipped)
+        }
     }
 
     const symbolsComp = {
@@ -18,7 +27,7 @@ export const Card = ({value, symbol} : any) => {
 
 
     return (
-        <div className="h-[20vh] w-[14.25vh]">
+        <div className={`h-[${height}vh] w-[${width}vh]`}>
             <div className={`h-full w-full [transition:transform_1s] cursor-pointer relative [transform-style:preserve-3d] ${isFlipped ? "[transform:rotateY(180deg)]" : ""}`} onClick={Flip}>
                 <div className={`absolute w-full h-full border-2 border-black overflow-hidden rounded-lg [-webkit-backface-visibility:hidden] [backface-visibility:hidden] justify-center items-center`} >
                     <div className='flex justify-center items-center h-full w-full text-xl'>
