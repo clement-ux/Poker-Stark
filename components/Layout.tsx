ccf95cb7e5e989d7e0a46d15dab37441695ca77a
LucasRamadour <72853747+LucasRamadour@users.noreply.github.com>
2022-07-08 19:40:43 +0200

    force icon height layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -22,38 +22,38 @@ export const Layout = ({children} :any) => {
     return(<div className="min-h-[100vh] w-full ">
             <Header/>
             <div className="flex h-[92vh] w-full ">
-                <div className={`flex flex-col ${showSide ? "w-96" : "w-[8vh]"} bg-gray-900 pl-6 ease-in duration-300 text-white space-y-14`}>
+                <div className={`flex flex-col ${showSide ? "w-96" : "w-[8vh]"} bg-gray-900 pl-6 ease-in duration-300 text-white space-y-14 z-10`}>
                     <div className="flex bg-gray-900 hover:bg-gray-500 rounded-[3px] h-10 w-10 items-center justify-center cursor-pointer text-white" onClick={() => changeShowSide()}>
                         <AiOutlineMenu size={20}/>
                     </div>
                     <div className="flex flex-row">
                     {
                         componentToShow !== "" &&
-                        <div className="flex bg-gray-900 hover:bg-gray-500 rounded-[3px] h-10 w-10 items-center justify-center cursor-pointer text-white" onClick={() => setComponentToShow("")}>
+                        <div className="flex bg-gray-900 hover:bg-gray-500 rounded-[3px] h-10 w-10 items-center justify-center cursor-pointer text-white mr-4" onClick={() => setComponentToShow("")}>
                             <AiOutlineArrowLeft size={20}/>
                         </div>
                     }
                     <div className= {`flex flex-col h-auto w-full space-y-4`}>
                         { (componentToShow == "bestHands" || componentToShow == "") &&
-                            <div className="flex flex-row">
+                            <div className="flex flex-row h-[35px]">
                                 <div className={`flex ${showSide ? "row-span-2" : "row-span-1"} cursor-pointer`} onClick={()=>{setComponentToShow("bestHands"), setShowSide(true)}}>
-                                <img className="h-auto w-[40px] mr-4 ml-4" src='/Images/CardsIcons.png'/>
+                                    <img className="h-[35px] w-auto mr-4" src='/Images/CardsIcons.png'/>
                                 </div>
                                 <div className={`flex ${showSide ? "text-white transition ease-in duration-300 delay-300 row-span-2" : "text-white/0"} cursor-pointer overflow-hidden`} onClick={()=>setComponentToShow("bestHands")}>{showSide && "Best Hands"}</div>
                             </div>
                         }
                         { (componentToShow == "" || componentToShow == "") &&
-                            <div className="flex flex-row">
+                            <div className="flex flex-row h-[35px]">
                                 <div className={`flex cursor-pointer`}>
-                                    <img className="h-auto w-[40px] mr-4 ml-4" src='/Images/CardsIcons.png'/>
+                                    <img className="h-[35px] w-auto mr-4" src='/Images/CardsIcons.png'/>
                                 </div>
                                 <div className={`flex ${showSide ? "text-white transition ease-in duration-300 delay-300 row-span-2" : "text-white/0"} cursor-pointer overflow-hidden`}>{showSide && "Best Hands"}</div>
                             </div>
                         }
                         { (componentToShow == "" || componentToShow == "") &&
-                            <div className="flex flex-row">
+                            <div className="flex flex-row h-[35px]">
                                 <div className={`flex cursor-pointer`}>
-                                    <img className="h-auto w-[40px] mr-4 ml-4" src='/Images/CardsIcons.png'/>
+                                    <img className="h-[35px] w-auto mr-4" src='/Images/CardsIcons.png'/>
                                 </div>
                                 <div className={`flex ${showSide ? "text-white transition ease-in duration-300 delay-300 row-span-2" : "text-white/0"} cursor-pointer overflow-hidden`}>{showSide && "Best Hands"}</div>
                             </div>
