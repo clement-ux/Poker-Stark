aaf8794bee67429dd06ec61b05ea67e8393cba0d
LucasRamadour <72853747+LucasRamadour@users.noreply.github.com>
2022-07-10 20:44:28 +0200

    card size 2 + best hands

--- components/BestHands.tsx
+++ components/BestHands.tsx
@@ -6,96 +6,99 @@ export const BestHands = () => {
 
     return (
 
-        <div> Poker Hands Ranking
+        <div className="flex flex-col space-y-8"> 
+            <div>
+                Poker Hands Ranking
+            </div>
 
-            <div className="divide-y divide-slate-900 ...">
+            <div className="divide-y divide-slate-900 ... space-y-6 pb-8">
                 <div>Royal Flush
-                    <div className="flex flex-row space-x-1">
-                        <Card value="10" symbol="H" size={3} />
-                        <Card value="J" symbol="H" size={3} />
-                        <Card value="Q" symbol="H" size={3} />
-                        <Card value="K" symbol="H" size={3} />
-                        <Card value="A" symbol="H" size={3} />
+                    <div className="flex flex-row space-x-2">
+                        <Card value="10" symbol="H" size={2} />
+                        <Card value="11" symbol="H" size={2} />
+                        <Card value="12" symbol="H" size={2} />
+                        <Card value="13" symbol="H" size={2} />
+                        <Card value="14" symbol="H" size={2} />
                     </div>
                 </div>
                 <div>Straight Flush
-                    <div className="flex flex-row space-x-1">
-                        <Card value="4" symbol="C" size={3} />
-                        <Card value="5" symbol="C" size={3} />
-                        <Card value="6" symbol="C" size={3} />
-                        <Card value="7" symbol="C" size={3} />
-                        <Card value="8" symbol="C" size={3} />
+                    <div className="flex flex-row space-x-2">
+                        <Card value="4" symbol="C" size={2} />
+                        <Card value="5" symbol="C" size={2} />
+                        <Card value="6" symbol="C" size={2} />
+                        <Card value="7" symbol="C" size={2} />
+                        <Card value="8" symbol="C" size={2} />
                     </div>
                 </div>
                 <div>Four of a Kind
-                    <div className="flex flex-row space-x-1">
-                        <Card value="A" symbol="C" size={3} />
-                        <Card value="A" symbol="H" size={3} />
-                        <Card value="A" symbol="S" size={3} />
-                        <Card value="A" symbol="D" size={3} />
+                    <div className="flex flex-row space-x-2">
+                        <Card value="14" symbol="C" size={2} />
+                        <Card value="14" symbol="H" size={2} />
+                        <Card value="14" symbol="S" size={2} />
+                        <Card value="14" symbol="D" size={2} />
                     </div>
                 </div>
                 <div>Full House
-                    <div className="flex flex-row space-x-1">
-                        <Card value="Q" symbol="C" size={3} />
-                        <Card value="Q" symbol="H" size={3} />
-                        <Card value="Q" symbol="S" size={3} />
-                        <Card value="7" symbol="C" size={3} />
-                        <Card value="7" symbol="D" size={3} />
+                    <div className="flex flex-row space-x-2">
+                        <Card value="12" symbol="C" size={2} />
+                        <Card value="12" symbol="H" size={2} />
+                        <Card value="12" symbol="S" size={2} />
+                        <Card value="7" symbol="C" size={2} />
+                        <Card value="7" symbol="D" size={2} />
                     </div>
                 </div>
                 <div>Flush
-                    <div className="flex flex-row space-x-1">
-                        <Card value="2" symbol="D" size={3} />
-                        <Card value="4" symbol="D" size={3} />
-                        <Card value="9" symbol="D" size={3} />
-                        <Card value="7" symbol="D" size={3} />
-                        <Card value="6" symbol="D" size={3} />
+                    <div className="flex flex-row space-x-2">
+                        <Card value="2" symbol="D" size={2} />
+                        <Card value="4" symbol="D" size={2} />
+                        <Card value="9" symbol="D" size={2} />
+                        <Card value="7" symbol="D" size={2} />
+                        <Card value="6" symbol="D" size={2} />
                     </div>
                 </div>
                 <div>Straight
-                    <div className="flex flex-row space-x-1">
-                        <Card value="4" symbol="C" size={3} />
-                        <Card value="5" symbol="D" size={3} />
-                        <Card value="6" symbol="S" size={3} />
-                        <Card value="7" symbol="D" size={3} />
-                        <Card value="8" symbol="C" size={3} />
+                    <div className="flex flex-row space-x-2">
+                        <Card value="4" symbol="C" size={2} />
+                        <Card value="5" symbol="D" size={2} />
+                        <Card value="6" symbol="S" size={2} />
+                        <Card value="7" symbol="D" size={2} />
+                        <Card value="8" symbol="C" size={2} />
                     </div>
                 </div>
                 <div>Three of a Kind
-                    <div className="flex flex-row space-x-1">
-                        <Card value="K" symbol="C" size={3} />
-                        <Card value="K" symbol="S" size={3} />
-                        <Card value="K" symbol="D" size={3} />
-                        <Card value="6" symbol="C" size={3} />
-                        <Card value="2" symbol="H" size={3} />
+                    <div className="flex flex-row space-x-2">
+                        <Card value="13" symbol="C" size={2} />
+                        <Card value="13" symbol="S" size={2} />
+                        <Card value="13" symbol="D" size={2} />
+                        <Card value="6" symbol="C" size={2} />
+                        <Card value="2" symbol="H" size={2} />
                     </div>
                 </div>
                 <div>Two Pair
-                    <div className="flex flex-row space-x-1">
-                        <Card value="3" symbol="C" size={3} />
-                        <Card value="3" symbol="H" size={3} />
-                        <Card value="9" symbol="S" size={3} />
-                        <Card value="9" symbol="D" size={3} />
-                        <Card value="5" symbol="C" size={3} />
+                    <div className="flex flex-row space-x-2">
+                        <Card value="3" symbol="C" size={2} />
+                        <Card value="3" symbol="H" size={2} />
+                        <Card value="9" symbol="S" size={2} />
+                        <Card value="9" symbol="D" size={2} />
+                        <Card value="5" symbol="C" size={2} />
                     </div>
                 </div>
                 <div>Pair
-                    <div className="flex flex-row space-x-1">
-                        <Card value="3" symbol="H" size={3} />
-                        <Card value="3" symbol="C" size={3} />
-                        <Card value="6" symbol="S" size={3} />
-                        <Card value="2" symbol="H" size={3} />
-                        <Card value="9" symbol="D" size={3} />
+                    <div className="flex flex-row space-x-2">
+                        <Card value="3" symbol="H" size={2} />
+                        <Card value="3" symbol="C" size={2} />
+                        <Card value="6" symbol="S" size={2} />
+                        <Card value="2" symbol="H" size={2} />
+                        <Card value="9" symbol="D" size={2} />
                     </div>
                 </div>
                 <div>High Card
-                    <div className="flex flex-row space-x-1">
-                        <Card value="4" symbol="D" size={3} />
-                        <Card value="2" symbol="C" size={3} />
-                        <Card value="K" symbol="H" size={3} />
-                        <Card value="7" symbol="S" size={3} />
-                        <Card value="9" symbol="C" size={3} />
+                    <div className="flex flex-row space-x-2">
+                        <Card value="4" symbol="D" size={2} />
+                        <Card value="2" symbol="C" size={2} />
+                        <Card value="13" symbol="H" size={2} />
+                        <Card value="7" symbol="S" size={2} />
+                        <Card value="9" symbol="C" size={2} />
                     </div>
                 </div>
             </div>
--- components/Card.tsx
+++ components/Card.tsx
@@ -4,7 +4,7 @@ import { getCard } from '../utils/getCard'
 
 export const Card = ({value, symbol, size, flippable, flipped} : any) => {
 
-    if(![3,7,8,10].includes(size)){
+    if(![2,3,7,8,10].includes(size)){
         size = 10
     }
 
@@ -13,6 +13,7 @@ export const Card = ({value, symbol, size, flippable, flipped} : any) => {
     console.log(cardUrl)
 
     const cardSizeStyle = {
+        2 : "h-[4vh] w-[3.1vh]",
         3 : "h-[6vh] w-[4.3vh]",
         7 : "h-[14vh] w-[9.9vh]",
         8 : "h-[16vh] w-[11.3vh]",
